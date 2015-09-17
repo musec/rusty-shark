575339ceb135deb7881ee02359bb8ca8fab66277
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-09-17 11:31:24 -0230

    Update docs.

--- implementors/core/ops/trait.Drop.js
+++ implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['pcap'] = ["impl&lt;T: <a class='trait' href='pcap/trait.State.html' title='pcap::State'>State</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='pcap/struct.Capture.html' title='pcap::Capture'>Capture</a>&lt;T&gt;","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='pcap/struct.Savefile.html' title='pcap::Savefile'>Savefile</a>",];
+implementors['pcap'] = ["impl&lt;T: <a class='trait' href='pcap/trait.State.html' title='pcap::State'>State</a> + ?<a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Sized.html' title='core::marker::Sized'>Sized</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='pcap/struct.Capture.html' title='pcap::Capture'>Capture</a>&lt;T&gt;","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='pcap/struct.Savefile.html' title='pcap::Savefile'>Savefile</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
