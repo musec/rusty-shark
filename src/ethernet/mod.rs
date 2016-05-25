181138c415aebf92acb25228c8dc32e519b5b8c4
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-05-25 17:14:03 -0230

    Refactor Ethernet module in separate directory.
    Put the Ethernet code in an 'ethernet' directory and move the test protocol
    parser to its own file within that directory.

---
+++ src/ethernet/mod.rs
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2015-2016 Jonathan Anderson
+ *
+ * Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+ * http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+ * http://opensource.org/licenses/MIT>, at your option. This file may not be
+ * copied, modified, or distributed except according to those terms.
+ */
+
+//! Dissection of Ethernet (IEEE 802.3) frames.
+
+use {
+    Endianness,
+    Error,
+    NamedValue,
+    Protocol,
+    RawBytes,
+    Result,
+    Val,
+    ip,
+    unsigned,
+};
+
+
+/// The IEEE 802.3 Ethernet protocol.
+pub struct Ethernet;
+
+
+/// Parse a six-byte MAC address, which can be encoded as, e.g., ff:ff:ff:ff:ff:ff.
+pub fn mac_address(raw: &[u8]) -> Result<Val> {
+    if raw.len() != 6 {
+        return Err(Error::InvalidData(format!["MAC address should have 6 B, not {}",
+                                              raw.len()]));
+    }
+
+    let encoded = raw
+                  .iter()
+                  .map(|b| format!["{:02x}", b])
+                  .collect::<Vec<String>>()
+                  .join(":")
+                  ;
+
+
+    Ok(Val::Address { bytes: raw.to_vec(), encoded: encoded })
+}
+
+
+impl Protocol for Ethernet {
+    fn short_name(&self) -> &str { "Ethernet" }
+    fn full_name(&self) -> &str { "IEEE 802.3 Ethernet" }
+    fn dissect(&self, data: &[u8]) -> Result {
+        if data.len() < 14 {
+            return Err(Error::Underflow { expected: 14, have: data.len(),
+                message: "An Ethernet frame must be at least 14 B".to_string() })
+        }
+
+        let mut values:Vec<NamedValue> = vec![];
+        values.push(("Destination".to_string(), mac_address(&data[0..6])));
+        values.push(("Source".to_string(), mac_address(&data[6..12])));
+
+        // The type/length field might be either a type or a length.
+        let tlen = unsigned(&data[12..14], Endianness::BigEndian);
+        let remainder = &data[14..];
+
+        match tlen {
+            Ok(i) if i <= 1500 => {
+                values.push(("Length".to_string(), Ok(Val::Unsigned(i))));
+            },
+
+            Ok(i) => {
+                let protocol: Box<Protocol> = match i {
+                    // TODO: use the simple 'box' syntax once it hits stable
+                    0x800 => Box::new(ip::IPv4),
+                    0x806 => RawBytes::boxed("ARP", "Address Resolution Protocol"),
+                    0x8138 => RawBytes::boxed("IPX", "Internetwork Packet Exchange"),
+                    0x86dd => RawBytes::boxed("IPv6", "Internet Protocol version 6"),
+
+                    0x9000 => Box::new(testproto::TestProtocol),
+
+                    _ => Box::new(RawBytes::unknown_protocol(&format!["0x{:x}", i])),
+                };
+
+                let protoname = protocol.short_name().to_string();
+                let description = protocol.full_name().to_string();
+
+                values.push(("Type".to_string(), Ok(Val::String(protoname))));
+                values.push((description, protocol.dissect(remainder)));
+            },
+            Err(e) => {
+                values.push(("Type/length".to_string(), Err(e)));
+            },
+        };
+
+        Ok(Val::Subpacket(values))
+    }
+}
+
+mod testproto;
--- src/ethernet/testproto.rs
+++ src/ethernet/testproto.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Jonathan Anderson
+ * Copyright 2015-2016 Jonathan Anderson
  *
  * Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
  * http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
@@ -7,93 +7,18 @@
  * copied, modified, or distributed except according to those terms.
  */
 
-//! Dissection of Ethernet (IEEE 802.3) frames.
+//! Dissection of the test protocol from the Xerox Blue Book (but not IEEE 802.3).
 
 use {
     Endianness,
     Error,
     NamedValue,
     Protocol,
-    RawBytes,
     Result,
     Val,
-    ip,
+    ethernet,
     unsigned,
 };
-
-
-/// The IEEE 802.3 Ethernet protocol.
-pub struct Ethernet;
-
-
-/// Parse a six-byte MAC address, which can be encoded as, e.g., ff:ff:ff:ff:ff:ff.
-pub fn mac_address(raw: &[u8]) -> Result<Val> {
-    if raw.len() != 6 {
-        return Err(Error::InvalidData(format!["MAC address should have 6 B, not {}",
-                                              raw.len()]));
-    }
-
-    let encoded = raw
-                  .iter()
-                  .map(|b| format!["{:02x}", b])
-                  .collect::<Vec<String>>()
-                  .join(":")
-                  ;
-
-
-    Ok(Val::Address { bytes: raw.to_vec(), encoded: encoded })
-}
-
-
-impl Protocol for Ethernet {
-    fn short_name(&self) -> &str { "Ethernet" }
-    fn full_name(&self) -> &str { "IEEE 802.3 Ethernet" }
-    fn dissect(&self, data: &[u8]) -> Result {
-        if data.len() < 14 {
-            return Err(Error::Underflow { expected: 14, have: data.len(),
-                message: "An Ethernet frame must be at least 14 B".to_string() })
-        }
-
-        let mut values:Vec<NamedValue> = vec![];
-        values.push(("Destination".to_string(), mac_address(&data[0..6])));
-        values.push(("Source".to_string(), mac_address(&data[6..12])));
-
-        // The type/length field might be either a type or a length.
-        let tlen = unsigned(&data[12..14], Endianness::BigEndian);
-        let remainder = &data[14..];
-
-        match tlen {
-            Ok(i) if i <= 1500 => {
-                values.push(("Length".to_string(), Ok(Val::Unsigned(i))));
-            },
-
-            Ok(i) => {
-                let protocol: Box<Protocol> = match i {
-                    // TODO: use the simple 'box' syntax once it hits stable
-                    0x800 => Box::new(ip::IPv4),
-                    0x806 => RawBytes::boxed("ARP", "Address Resolution Protocol"),
-                    0x8138 => RawBytes::boxed("IPX", "Internetwork Packet Exchange"),
-                    0x86dd => RawBytes::boxed("IPv6", "Internet Protocol version 6"),
-
-                    0x9000 => Box::new(TestProtocol),
-
-                    _ => Box::new(RawBytes::unknown_protocol(&format!["0x{:x}", i])),
-                };
-
-                let protoname = protocol.short_name().to_string();
-                let description = protocol.full_name().to_string();
-
-                values.push(("Type".to_string(), Ok(Val::String(protoname))));
-                values.push((description, protocol.dissect(remainder)));
-            },
-            Err(e) => {
-                values.push(("Type/length".to_string(), Err(e)));
-            },
-        };
-
-        Ok(Val::Subpacket(values))
-    }
-}
 
 
 /// Testing protocol from the Xerox Blue Book.
@@ -177,7 +102,7 @@ impl <'a> TestMessage <'a> {
 
             Ok(2) => Ok({
                 TestMessage::ForwardData{
-                    dest: mac_address(&data[2..8]),
+                    dest: ethernet::mac_address(&data[2..8]),
                     message: TestMessage::parse(&data[8..]).map(Box::new),
                 }
             }),
